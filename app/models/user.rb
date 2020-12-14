a46501aa71529265b40f2e66254972582b5c5612
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-14 14:36:12 +0900

    モデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #has_many :events
+  #has_many :join
+  #has_many :comments
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
 end
