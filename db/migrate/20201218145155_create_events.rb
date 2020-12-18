c9419f62698d196e9c7259c325fa80908e026b4a
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-19 00:09:23 +0900

    eventテーブルのカラム名設定

--- db/migrate/20201218145155_create_events.rb
+++ db/migrate/20201218145155_create_events.rb
@@ -1,6 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.string     :title,       null: false
+      t.date       :date,        null: false
+      t.string     :explanation, null: false
+      t.references :user,        null: false, foreign_key: true
+      t.integer    :location_id, null: false
+      t.integer    :category_id, null: false
 
       t.timestamps
     end
