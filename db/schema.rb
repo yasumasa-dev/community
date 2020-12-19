117b4bd4711c68bda570672342c4dce47ee12fbd
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-19 23:41:16 +0900

    テーブルカラム設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_132107) do
+ActiveRecord::Schema.define(version: 2020_12_18_145155) do
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title", null: false
+    t.date "date", null: false
+    t.string "explanation", null: false
+    t.bigint "user_id", null: false
+    t.integer "location_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_12_11_132107) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
