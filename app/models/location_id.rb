ccd3e977d2e13bbf4d44c599eeafb084c098b568
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-19 23:45:05 +0900

    アクティブハッシュ

--- app/models/location_id.rb
+++ app/models/location_id.rb
@@ -1,2 +1,11 @@
 class LocationId < ApplicationRecord
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'オンライン' },
+    { id: 3, name: '高知市内' },
+    { id: 4, name: '県外' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :events
 end
