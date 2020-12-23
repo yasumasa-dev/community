bea77ad3ae3ce002795e026a65ee485178e3f68f
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-23 22:33:08 +0900

    eventモデルアソシエーション設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :location_id
+
+  belongs_to :user
+  has_one_attached :image
 end
