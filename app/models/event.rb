8005602b5d61386092510f7b98dbe6ccf562e7ca
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-23 22:33:40 +0900

    バリデーションの設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,7 @@ class Event < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  validates :title, :explanation, :image, presence: true
+
+  validates :category_id, :location_id, numericality: { other_than: 1 } 
 end
